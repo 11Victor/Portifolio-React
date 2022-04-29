ee1b785a904387cc6804cef4c1950877a1a60dad
11Victor <victor.alcantara@unionitdigital.com.br>
2022-04-29 01:30:32 -0300

    Adicionando icones

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Intro.css'
+import GitHub from '../../img/github.png';
+import Linkedin from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
 
 const Intro = () => {
   return (
@@ -8,9 +11,14 @@ const Intro = () => {
             <div className="i-name">
                 <span>Olá! Eu sou </span>
                 <span>Victor Valencio</span>
-                <span>Desenvolvedor Full Stack com alto nível de conhecimento e experiência em Back-end, Front-end, Database e DevOps, produzindo com alta performance!</span>
+                <span>Desenvolvedor Full Stack com alto nível de conhecimento e experiência em Back-end, Front-end, Database e DevOps!</span>
             </div>
             <button className="button i-button">Contrate-me</button>
+            <div className="i-icons">
+              <img src={GitHub} alt="" />
+              <img src={Linkedin} alt="" />
+              <img src={Instagram} alt="" />
+            </div>
         </div>
         <div className="i-right">
             Texto teste
